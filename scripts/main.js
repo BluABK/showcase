cb6934f4f529ea96cdd2765910ee7392cc3607d1
BluABK <bluabk@bluabk.net>
2021-04-09 23:18:26 +0200

    Drop down menu and loading HTML files into content section now works!

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 // Model
+// let dropdownOptions = 
+let href = "../placeholder.html";
+let content = "Please select content from the dropdown menu.";
 
 // View
 function updateViews() {
@@ -8,16 +11,41 @@ function updateViews() {
 
 function updateViewTopBar() {
     document.getElementById("top-bar").innerHTML = `
-        test
+        <div id="dropdown" class="dropbtn" onClick="showDropdownMenu(this)">Load task…<div>
+            <div id="myDropdown" class="dropdown-content" onClick="selectDropdownOption(event.target)"></div>
+        </div>
     `;
+
+    $("#myDropdown").load("includes.html"); 
 }
 
 function updateViewContent() {
     document.getElementById("app").innerHTML = `
-        asdf
+        ${content}
     `;
 }
 
 // Controller
+function loadContent(path, selector="") {
+    // Append a whitespace to selector if used.
+    if (selector != "") selector = ' ' + selector;
+
+    // Create a temporary DIV element in memory and use load() on it,
+    // in order to load the contents of a file into it using JQuery.
+    var $div = $('<div>');
+    $div.load(`${path}${selector}`, function(){
+        content = $(this)[0].innerHTML;
+
+        updateViews();
+    });
+}
+
+function showDropdownMenu(a) {
+    a.parentNode.getElementsByClassName('dropdown-content')[0].classList.toggle("show");
+}
+
+function selectDropdownOption(element) {
+    content = loadContent(element.getAttribute("href"));
+}
 
 updateViews();
